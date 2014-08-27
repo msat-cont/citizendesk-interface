2fc455e897c66bf36509f4ef0f078a96737b529b
Martin Saturka <martin.saturka@sourcefabric.org>
2014-08-27 13:07:10 +0200

    phrases for report statuses, as being used e.g. for liveblog output

---
+++ initialise/report_statuses.py
@@ -0,0 +1,25 @@
+from main import Init, init_collection
+
+report_statuses = [{
+        'name': 'new',
+        'phrasing': 'This item has been received.',
+}, {
+        'name': 'assigned',
+        'phrasing': 'This item has been assigned for verification.',
+}, {
+        'name': 'dismissed',
+        'phrasing': 'This item has been dismissed and irrelevant.',
+}, {
+        'name': 'false',
+        'phrasing': 'This item has been found false.',
+}, {
+        'name': 'verified',
+        'phrasing': 'This item has been verified.',
+}]
+
+for status in report_statuses:
+    status['phrasing_original'] = status['phrasing']
+
+init_report_statuses = Init('report_status', report_statuses)
+
+if __name__ == "__main__": init_collection(init_report_statuses)
